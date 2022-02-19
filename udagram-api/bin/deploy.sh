16695311b85e7b5e0893b9f1fea64044897c5ebd
mahmoud.bakhit@vodafone.com <mahmoud.bakhit@vodafone.com>
2022-02-19 03:10:29 +0200

    api deploy sh file

---
+++ udagram-api/bin/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+echo "initializing elastic Beanstalk repo..."
+eb init udagram-backend -p "64bit Amazon Linux 2 v5.4.10 running Node.js 14" --region us-east-1
+
+echo "create environement and set it as default..."
+eb create -d udagram-api-dev
+
+echo "connects to the created Elastic Beanstalk env..."
+eb use udagram-api-dev
+
+echo "deploy app to Elastaic Beanstalk environement..."
+eb deploy udagram-api-dev
+
+echo "Set environment variables...."
+
+eb setenv AWS_BUCKET=$AWS_BUCKET AWS_REGION=$AWS_REGION DB_PORT=$DB_PORT JWT_SECRET=$JWT_SECRET PORT=$PORT POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME URL=$URL
