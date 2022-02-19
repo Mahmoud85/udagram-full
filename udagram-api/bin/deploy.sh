81ce7a48023df0c1468801048d7c816dfa6f06c2
mahmoud.bakhit@vodafone.com <mahmoud.bakhit@vodafone.com>
2022-02-19 03:51:52 +0200

    add circle ci and fix package json

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -8,9 +8,10 @@ eb create -d udagram-api-dev
 echo "connects to the created Elastic Beanstalk env..."
 eb use udagram-api-dev
 
-echo "deploy app to Elastaic Beanstalk environement..."
-eb deploy udagram-api-dev
-
 echo "Set environment variables...."
 
 eb setenv AWS_BUCKET=$AWS_BUCKET AWS_REGION=$AWS_REGION DB_PORT=$DB_PORT JWT_SECRET=$JWT_SECRET PORT=$PORT POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME URL=$URL
+
+echo "deploy app to Elastaic Beanstalk environement..."
+eb deploy udagram-api-dev
+
